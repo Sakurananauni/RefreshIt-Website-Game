eedae071927a53d1770ee7bf8e573ea0296334b1
Mimi <53148222+Sakurananauni@users.noreply.github.com>
2020-06-15 16:03:34 +0530

    Added Initial files

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+var randomNumber1 = Math.floor(Math.random() *6) +1;
+var randomNumber2 = Math.floor(Math.random() *6) +1;
+
+
+var randomImage= "dice" + randomNumber1 + ".png";
+var randomImage1= "dice" + randomNumber2 + ".png";
+
+var imageSource= "images/" + randomImage;
+var imageSource1= "images/" + randomImage1;
+
+var image1= document.querySelectorAll("img")[0];
+var image2= document.querySelectorAll("img")[1];
+
+
+image1.setAttribute("src",imageSource);
+image2.setAttribute("src",imageSource1);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="PLAYER 1 WINS╰(*°▽°*)╯";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="PLAYER 2 WINS╰(*°▽°*)╯";
+}
+else{
+    document.querySelector("h1").innerHTML="DRAW¯\_(ツ)_/¯";
+}
+
